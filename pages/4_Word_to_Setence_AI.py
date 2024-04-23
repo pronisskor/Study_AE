92c54c90f98c4040649ba4b401640adb00245b06
pronisskor <163367282+pronisskor@users.noreply.github.com>
2024-04-23 10:26:46 +0900

    Update 4_Word_to_Setence_AI.py

--- pages/4_Word_to_Setence_AI.py
+++ pages/4_Word_to_Setence_AI.py
@@ -22,9 +22,9 @@ if openai_api_key:
 if 'start' not in st.session_state:
     st.session_state['start'] = False
 
-if 'words_list' not in st.session_state or 'learned_count' not in st.session_state:
-    st.session_state['words_list'] = []
-    st.session_state['learned_count'] = 0
+if 'ai_words_list' not in st.session_state or 'ai_learned_count' not in st.session_state:
+    st.session_state['ai_words_list'] = []
+    st.session_state['ai_learned_count'] = 0
 
 def load_words():
     file_name = 'http://ewking.kr/AE/word_sentence.xlsx'
@@ -34,8 +34,8 @@ def load_words():
         df = pd.read_excel(file_name)
     words_column = 'words'
     if df is not None and words_column in df.columns:
-        st.session_state['words_list'] = df[words_column].dropna().tolist()
-        random.shuffle(st.session_state['words_list'])
+        st.session_state['ai_words_list'] = df[words_column].dropna().tolist()
+        random.shuffle(st.session_state['ai_words_list'])
 
 def generate_sentence_with_word(word):
     try:
@@ -65,29 +65,29 @@ def generate_sentence_with_word(word):
 def restart_study():
     if st.button('New Generate'):
         st.session_state['start'] = False
-        st.session_state['words_list'] = []
-        st.session_state['learned_count'] = 0
+        st.session_state['ai_words_list'] = []
+        st.session_state['ai_learned_count'] = 0
         load_words()
         st.session_state['start'] = True
 
 restart_study()
 
-if st.session_state['start'] and st.session_state.get('words_list'):
-    random_word = st.session_state['words_list'].pop(0)
-    st.session_state['learned_count'] += 1  # 학습한 단어 카운트 증가
+if st.session_state['start'] and st.session_state.get('ai_words_list'):
+    random_word = st.session_state['ai_words_list'].pop(0)
+    st.session_state['ai_learned_count'] += 1  # 학습한 단어 카운트 증가
     with st.spinner('문장 생성중...'):
         english_sentence, korean_translation = generate_sentence_with_word(random_word)
         if english_sentence and korean_translation:
             highlighted_english_sentence = english_sentence.replace(random_word, f'<strong>{random_word}</strong>')
             st.markdown(f'<p style="font-size: 20px; text-align: center;">{highlighted_english_sentence}</p>', unsafe_allow_html=True)
             st.markdown(f'<p style="font-size: 20px; text-align: center;">{korean_translation}</p>', unsafe_allow_html=True)
-            st.markdown(f'공부한 단어 수: {st.session_state["learned_count"]}')  # 학습한 단어 수 표시
+            st.markdown(f'공부한 단어 수: {st.session_state["ai_learned_count"]}')  # 학습한 단어 수 표시
 
-    if st.button('다음 단어') or not st.session_state.get('words_list'):
-        if not st.session_state['words_list']:
+    if st.button('다음 단어') or not st.session_state.get('ai_words_list'):
+        if not st.session_state['ai_words_list']:
             st.markdown(f'<p style="background-color: #bffff2; padding: 10px;">모든 단어에 대한 문장을 생성했습니다.<br>다시 시작하려면 다시시작 버튼을 누르세요.</p>', unsafe_allow_html=True)
             st.session_state['start'] = False
-            st.session_state['words_list'] = []
-            st.session_state['learned_count'] = 0
+            st.session_state['ai_words_list'] = []
+            st.session_state['ai_learned_count'] = 0
             load_words()  # 다시 시작하려면 단어 목록을 다시 로드
             st.session_state['start'] = True
